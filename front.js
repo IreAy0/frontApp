796759ce49d858acad58fcb53fbff2f77d8dabb6
Confidence <confidenceigwe@gmail.com>
2019-05-19 04:34:04 +0100

    js file

---
+++ front.js
@@ -0,0 +1,26 @@
+
+ var d1 = document.getElementById('form1');
+ var d2 = document.getElementById('form2');
+ var d3 = document.getElementById('form3');
+
+function Swap()
+{
+   if( d2.style.display == "none" ){
+      d1.style.display = "none";
+      d2.style.display = "block";     
+   }
+   else 
+   {
+      d1.style.display = "block";
+      d2.style.display = "none";   
+   }
+}
+function SecondSwap(){
+    if( d3.style.display == "none" ){
+    d1.style.display = "none";
+    d3.style.display = "block";   
+}else{
+    d1.style.display = "block";
+      d3.style.display = "none";
+} 
+}
